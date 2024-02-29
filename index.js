1aaf1c4c6af2c4e3a4737e574dd57cf3ed45360c
Ekthasurya <suryakiranmajhi1999@gmail.com>
2024-02-29 13:03:52 +0530

    this is my 1st assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+//1
+console.log("Masai School");
+console.log("A Transformation of Education");
+
+//2
+var name = "Surya Kiran Majhi";
+console.log(name);
+name = "Haripada Majhi";
+console.log(name);
+name="Usha Lata Majhi";
+console.log(name);
+
+//3
+
+var name = "Surya Kiran Majhi";
+var age = 25;
+console.log("My name is "+ name + " And my age is "+ age+ ".");
+
+//4
+
+//5
+
+var num1 =5;
+var num2 =6;
+console.log(5===6);
+
+//6
+var num =30;
+var C_num = 28;
+console.log(C_num>num);
